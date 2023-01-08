fb0c310fe3f2dfd51d1f0bd4c49bf0078a203023
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2023-01-08 21:19:16 +0000

    Installed @sanity/client

--- sanitymedium/sanity.cli.ts
+++ sanitymedium/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'l0ow9nb5',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
